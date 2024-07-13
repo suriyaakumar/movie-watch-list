0f71ab66d4a47a4c116e930cf67c24dd12ba3646
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-14 01:08:01 +0530

    fix: fix logged in user check

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -11,11 +11,13 @@ export default function Login() {
         document.title = 'Watchlists | Home';
     }, []);
 
-    const handleSubmit = async (e) => {
+    const login = async (e) => {
         e.preventDefault();
         localStorage.setItem('user', e.target.email.value);
         const user = await getUser(email)
-        if(user === null) setUser(email)
+        if (!user) await setUser(email, {
+            watchlists: []
+        });
         navigate('home');
     }
 
@@ -23,9 +25,9 @@ export default function Login() {
         <div className="container min-h-screen mx-auto">
             <div className="flex flex-col w-11/12 lg:w-4/12 mx-auto space-y-5 items-center justify-center">
                 <h1 className="font-extrabold tracking-tight text-5xl text-red-600">Watchlists</h1>
-                <form onSubmit={handleSubmit} className="w-full flex flex-col space-y-3 items-center justify-center">
+                <form onSubmit={login} className="w-full flex flex-col space-y-3 items-center justify-center">
                     <label className='w-full' htmlFor='email'>Email Address</label>
-                    <input name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="w-full border-gray-400 shadow-md py-2 px-3 rounded" type="email" autoComplete="email" placeholder="" />
+                    <input name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="w-full focus:outline-none border-2 py-2 px-3 rounded" type="email" autoComplete="email" placeholder="" />
                     <button className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign in with Email</button>
                 </form>
             </div>

d1ebbc1314536b650b58e905e50c57213cb6e85e
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-15 02:10:48 +0530

    fix: layout dimensions on smaller screens

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,38 +1,48 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSwipeable } from 'react-swipeable';
 import { List, X } from '@phosphor-icons/react';
 import NavBar from './navbar';
+import DatabaseContext from '../db/dbContext';
 
 export default function Layout() {
 	const [menuOpen, setMenuOpen] = useState(false);
-
 	const handlers = useSwipeable({
 		onSwipedLeft: () => setMenuOpen(!menuOpen),
 		onSwipedRight: () => setMenuOpen(!menuOpen),
 		trackMouse: true,
 		preventDefaultTouchmoveEvent: true,
 	});
+	const [currentUser, setCurrentUser] = useState({});
+	const { getUser } = useContext(DatabaseContext);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const user = await getUser(localStorage.getItem('user'));
+			setCurrentUser(user);
+		};
+		fetchUser();
+	}, [getUser]);
 
 	return (
 		<div className='flex'>
 			<div
 				{...handlers}
-				className={`fixed inset-y-0 left-0 w-4/12 lg:w-2/12 bg-white transform ${menuOpen ? 'translate-x-0 border-1 shadow-lg' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
+				className={`fixed inset-y-0 left-0 w-full sm:w-5/12 lg:w-2/12 bg-white transform ${menuOpen ? 'translate-x-0 border-1 shadow-lg' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
 			>
-				<div className='space-y-4 p-4'>
+				<div className='h-full flex flex-col space-y-4 p-4'>
 					<div className='flex justify-between'>
-						<h2 className='tracking-tight text-2xl text-red-500 lg:text-3xl font-bold'>
+						<h1 className='tracking-tight text-2xl text-red-500 lg:text-3xl font-bold'>
 							Watchlists
-						</h2>
+						</h1>
 						<button
-							className='bg-red-500 text-white lg:hidden'
+							className='lg:hidden'
 							onClick={() => setMenuOpen(!menuOpen)}
 						>
 							<X size={28} />
 						</button>
 					</div>
-					<NavBar />
+					<NavBar user={currentUser} />
 				</div>
 			</div>
 

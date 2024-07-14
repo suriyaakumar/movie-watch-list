0d08c1ebc0e1031b08bcceb58d050e3f8f01b933
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-14 19:17:21 +0530

    refactor: repurpose sidebar and remove fetch logic for now

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,29 +1,41 @@
-import { Outlet } from "react-router-dom";
-import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { List, X } from "@phosphor-icons/react";
-import SideNavBar from "./sidenavbar";
+import { Outlet } from 'react-router-dom';
+import { List, X } from '@phosphor-icons/react';
+import NavBar from './navbar';
 
-export default function Layout({ watchlists }) {
-    const [menuOpen, setMenuOpen] = useState(false);
+export default function Layout() {
+	const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <div className="flex min-h-screen">
-            <div className="hidden lg:block lg:w-2/12 py-5 px-3 pb">
-                <SideNavBar lists={watchlists} />
-            </div>
-            <div className="w-full lg:w-10/12">
-                <button onClick={() => { setMenuOpen(!menuOpen); }} className={`lg:hidden ${menuOpen ? 'hidden' : 'block'} m-2 p-1 border-2 flex items-center`}><List size={28} /></button>
-                <div className={`fixed transform bg-white border-1 shadow-xl transition-transform duration-1000 ease-in-out ${menuOpen ? 'block translate-x-0' : 'hidden -translate-x-full'} w-4/12 h-screen px-3 z-40`}>
-                    <button onClick={() => { setMenuOpen(!menuOpen); }} className={`lg:hidden m-2 p-1`}><X size={24} /></button>
-                    <SideNavBar lists={watchlists} />
-                </div>
-                <Outlet />
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className='flex'>
+			<div
+				className={`fixed inset-y-0 left-0 w-4/12 lg:w-2/12 bg-white transform ${menuOpen ? 'translate-x-0 border-1 shadow-lg' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
+			>
+				<div className='space-y-4 p-4'>
+					<div className='flex justify-between'>
+						<h2 className='tracking-tight text-lg text-red-500 lg:text-3xl font-bold'>
+							Watchlists
+						</h2>
+						<button
+							className='bg-red-500 text-white lg:hidden'
+							onClick={() => setMenuOpen(!menuOpen)}
+						>
+							<X size={28} />
+						</button>
+					</div>
+					<NavBar />
+				</div>
+			</div>
 
-Layout.propTypes = {
-    watchlists: PropTypes.array
+			<main className='flex-1 p-4 ml-0 lg:ml-64 transition-all duration-300 ease-in-out'>
+				<button
+					className='lg:hidden p-1 bg-red-600 text-white'
+					onClick={() => setMenuOpen(!menuOpen)}
+				>
+					<List size={28} />
+				</button>
+				<Outlet />
+			</main>
+		</div>
+	);
 }

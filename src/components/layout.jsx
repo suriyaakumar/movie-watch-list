7543ff66e29065b397535a104786f4c385fb221e
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-16 00:04:16 +0530

    feat: use user provider in layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,9 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSwipeable } from 'react-swipeable';
 import { List, X } from '@phosphor-icons/react';
+import { UserProvider } from '../contexts/userContext';
 import NavBar from './navbar';
-import DatabaseContext from '../db/dbContext';
 
 export default function Layout() {
 	const [menuOpen, setMenuOpen] = useState(false);
@@ -13,48 +13,39 @@ export default function Layout() {
 		trackMouse: true,
 		preventDefaultTouchmoveEvent: true,
 	});
-	const [currentUser, setCurrentUser] = useState({});
-	const { getUser } = useContext(DatabaseContext);
-
-	useEffect(() => {
-		const fetchUser = async () => {
-			const user = await getUser(localStorage.getItem('user'));
-			setCurrentUser(user);
-		};
-		fetchUser();
-	}, [getUser]);
 
 	return (
-		<div className='flex'>
-			<div
-				{...handlers}
-				className={`fixed inset-y-0 left-0 w-full sm:w-5/12 lg:w-2/12 bg-white transform ${menuOpen ? 'translate-x-0 border-1 shadow-lg' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
-			>
-				<div className='h-full flex flex-col space-y-4 p-4'>
-					<div className='flex justify-between'>
-						<h1 className='tracking-tight text-2xl text-red-500 lg:text-3xl font-bold'>
-							Watchlists
-						</h1>
-						<button
-							className='lg:hidden'
-							onClick={() => setMenuOpen(!menuOpen)}
-						>
-							<X size={28} />
-						</button>
-					</div>
-					<NavBar user={currentUser} />
-				</div>
-			</div>
-
-			<main className='flex-1 p-4 ml-0 lg:ml-64 transition-all duration-300 ease-in-out'>
-				<button
-					className='lg:hidden p-1 bg-red-600 text-white'
-					onClick={() => setMenuOpen(!menuOpen)}
+		<UserProvider>
+			<div className='flex'>
+				<div
+					{...handlers}
+					className={`fixed inset-y-0 left-0 w-full h-full sm:w-4/12 lg:w-3/12 xl:w-2/12 bg-white transform ${menuOpen ? 'translate-x-0 border-1 shadow-lg' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
 				>
-					<List size={28} />
-				</button>
-				<Outlet />
-			</main>
-		</div>
+					<div className='h-full flex flex-col space-y-2'>
+						<div className='flex justify-between p-4'>
+							<h1 className='tracking-tight text-center text-2xl text-red-500 lg:text-3xl font-bold'>
+								Watchlists
+							</h1>
+							<button
+								className='lg:hidden'
+								onClick={() => setMenuOpen(!menuOpen)}
+							>
+								<X size={28} />
+							</button>
+						</div>
+						<NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+					</div>
+				</div>
+				<main className='flex-1 p-4 sm-ml-[calc(100vw/12*4)] lg:ml-[calc(100vw/12*3)] xl:ml-[calc(100vw/12*2)] transition-all duration-300 ease-in-out'>
+					<button
+						className='lg:hidden p-1 bg-red-600 text-white'
+						onClick={() => setMenuOpen(!menuOpen)}
+					>
+						<List size={28} />
+					</button>
+					<Outlet />
+				</main>
+			</div>
+		</UserProvider>
 	);
 }

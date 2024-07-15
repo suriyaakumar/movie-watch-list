d03a7b27d483d795233cb7b173aa44a715294eba
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-15 15:16:36 +0530

    fix: fix imports in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { DatabaseProvider } from './db/dbContext.jsx';
-import Home from './components/home.jsx';
-import Login from './components/login.jsx';
+import { DatabaseProvider } from './contexts/dbContext.jsx';
+import Home from './screens/home.jsx';
+import Login from './screens/login.jsx';
 import ProtectedRoute from './auth/protectedRoute.jsx';
+import Watchlist from './screens/watchlist.jsx';
 
 function App() {
-  return (
-    <DatabaseProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="home" element={<Home />} />
-          </Route>
-        </Routes>
-      </Router>
-    </DatabaseProvider>
-  )
+	return (
+		<DatabaseProvider>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Login />} />
+					<Route element={<ProtectedRoute />}>
+						<Route path='home' element={<Home />} />
+						<Route path='list/:id' element={<Watchlist />} />
+					</Route>
+				</Routes>
+			</Router>
+		</DatabaseProvider>
+	);
 }
 
-export default App
+export default App;

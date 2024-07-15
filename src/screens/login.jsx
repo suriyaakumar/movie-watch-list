45aabc865155ab65b1d3494a28e2f9354eb6dc15
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-15 15:04:09 +0530

    refactor: move screens into screens folder

---
+++ src/screens/login.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useContext } from 'react';
+import DatabaseContext from '../contexts/dbContext';
+import { useNavigate } from 'react-router-dom';
+import { SignIn } from '@phosphor-icons/react';
+
+export default function Login() {
+	const [email, setEmail] = useState('');
+	const { setUser, getUser } = useContext(DatabaseContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		document.title = 'Watchlists | Home';
+	}, []);
+
+	const login = async (e) => {
+		e.preventDefault();
+		localStorage.setItem('user', e.target.email.value);
+		const user = await getUser(email);
+		if (!user)
+			await setUser(email, {
+				email,
+				watchlists: [],
+			});
+		navigate('home');
+	};
+
+	return (
+		<div className='container min-h-screen mx-auto'>
+			<div className='flex flex-col w-11/12 lg:w-4/12 mx-auto space-y-5 items-center justify-center'>
+				<h1 className='font-extrabold tracking-tight text-5xl text-red-600'>
+					Watchlists
+				</h1>
+				<form
+					onSubmit={login}
+					className='w-full flex flex-col space-y-3 items-center justify-center'
+				>
+					<label className='w-full' htmlFor='email'>
+						Email Address
+					</label>
+					<input
+						name='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						className='w-full focus:outline-none border-2 py-2 px-3 rounded'
+						type='email'
+						autoComplete='email'
+						placeholder=''
+					/>
+					<button className='w-full flex justify-center space-x-1 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>
+						<SignIn className='h-6 w-6' />
+						<span>Sign in with Email</span>
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+}

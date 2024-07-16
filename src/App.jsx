3a09975fc5d78d32aa35052cfd0db9a03b3d5030
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-16 14:34:53 +0530

    feat: add routes for profile & watchlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DatabaseProvider } from './contexts/dbContext.jsx';
+import ProtectedRoute from './auth/protectedRoute.jsx';
 import Home from './screens/home.jsx';
 import Login from './screens/login.jsx';
-import ProtectedRoute from './auth/protectedRoute.jsx';
-import Watchlist from './screens/watchlist.jsx';
+import Profile from './screens/profile.jsx';
+import Watchlists from './screens/watchlists.jsx';
 
 function App() {
 	return (
 		<DatabaseProvider>
-			<Router>
-				<Routes>
-					<Route path='/' element={<Login />} />
-					<Route element={<ProtectedRoute />}>
-						<Route path='home' element={<Home />} />
-						<Route path='list/:id' element={<Watchlist />} />
-					</Route>
-				</Routes>
-			</Router>
+				<Router>
+					<Routes>
+						<Route path='/' element={<Login />} />
+						<Route element={<ProtectedRoute />}>
+							<Route path='home' element={<Home />} />
+							<Route path='profile' element={<Profile />} />
+							<Route path='watchlists' element={<Watchlists />} />
+						</Route>
+					</Routes>
+				</Router>
 		</DatabaseProvider>
 	);
 }

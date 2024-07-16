c488a0dad891031ffae21516678337a2938b4463
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-16 21:14:40 +0530

    feat: add react toastify to throw errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,39 @@ import ProtectedRoute from './auth/protectedRoute.jsx';
 import Home from './screens/home.jsx';
 import Login from './screens/login.jsx';
 import Profile from './screens/profile.jsx';
+import Movie from './screens/movie.jsx';
+import Watchlist from './screens/watchlist.jsx';
 import Watchlists from './screens/watchlists.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
 	return (
 		<DatabaseProvider>
-				<Router>
-					<Routes>
-						<Route path='/' element={<Login />} />
-						<Route element={<ProtectedRoute />}>
-							<Route path='home' element={<Home />} />
-							<Route path='profile' element={<Profile />} />
-							<Route path='watchlists' element={<Watchlists />} />
-						</Route>
-					</Routes>
-				</Router>
+			<ToastContainer
+				position='bottom-right'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme='colored'
+			/>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Login />} />
+					<Route element={<ProtectedRoute />}>
+						<Route path='home' element={<Home />} />
+						<Route path='movie' element={<Movie />} />
+						<Route path='profile' element={<Profile />} />
+						<Route path='watchlist/:id' element={<Watchlist />} />
+						<Route path='watchlists' element={<Watchlists />} />
+					</Route>
+				</Routes>
+			</Router>
 		</DatabaseProvider>
 	);
 }

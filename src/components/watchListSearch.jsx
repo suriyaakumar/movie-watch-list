7e2b46c0ff22b3a8912c04624b9cabcefe6d4d4d
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-17 21:04:37 +0530

    refactor: add popover to watchlist search

--- src/components/watchListSearch.jsx
+++ src/components/watchListSearch.jsx
@@ -1,7 +1,8 @@
 import { Command } from 'cmdk';
-import { Plus } from '@phosphor-icons/react';
+import { Plus, Bookmark } from '@phosphor-icons/react';
 import { useCreateWatchlist } from '../hooks/useCreateWatchList';
 import PropTypes from 'prop-types';
+import * as Popover from '@radix-ui/react-popover';
 
 export default function WatchListSearch({ currentUser, movie, onSelect }) {
 	const createWatchlist = useCreateWatchlist();
@@ -13,34 +14,43 @@ export default function WatchListSearch({ currentUser, movie, onSelect }) {
 	};
 
 	return (
-		<Command>
-			<Command.Input
-				placeholder='Search ...'
-				className='block px-4 py-2 focus:outline-none'
-			/>
-			<button
-				onClick={() => createWatchlist(movie)}
-				className='flex items-center w-full space-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
-			>
-				<Plus size={28} /> <span>New watchlist</span>
-			</button>
-			<Command.List className='overflow-y-auto h-28 xl:h-40'>
-				<Command.Empty className='w-11/12 mx-auto p-2 text-center tracking-tight'>
-					No watchlists found.
-				</Command.Empty>
-				{currentUser.watchlists &&
-					currentUser.watchlists.length > 0 &&
-					currentUser.watchlists.map((watchlist) => (
-						<Command.Item
-							onSelect={() => handleSelect(watchlist?.id, movie)}
-							key={watchlist?.id}
-							className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+		<Popover.Root>
+			<Popover.Trigger className='inline-flex rounded p-1.5 bg-red-600 hover:bg-red-600 text-white font-bold'>
+				<Bookmark className='h-5 w-5' />
+			</Popover.Trigger>
+			<Popover.Portal>
+				<Popover.Content className='bg-white rounded shadow-2xl border-2 mt-2'>
+					<Command>
+						<Command.Input
+							placeholder='Search ...'
+							className='block px-4 py-2 focus:outline-none'
+						/>
+						<button
+							onClick={() => createWatchlist(movie)}
+							className='flex items-center w-full space-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
 						>
-							{watchlist?.name}
-						</Command.Item>
-					))}
-			</Command.List>
-		</Command>
+							<Plus size={28} /> <span>New watchlist</span>
+						</button>
+						<Command.List className='overflow-y-auto h-28 xl:h-40'>
+							<Command.Empty className='w-11/12 mx-auto p-2 text-center tracking-tight'>
+								No watchlists found.
+							</Command.Empty>
+							{currentUser.watchlists &&
+								currentUser.watchlists.length > 0 &&
+								currentUser.watchlists.map((watchlist) => (
+									<Command.Item
+										onSelect={() => handleSelect(watchlist?.id, movie)}
+										key={watchlist?.id}
+										className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+									>
+										{watchlist?.name}
+									</Command.Item>
+								))}
+						</Command.List>
+					</Command>
+				</Popover.Content>
+			</Popover.Portal>
+		</Popover.Root>
 	);
 }
 

f356bdd817eb512302551b89f6e9e4a7edde30b6
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-17 17:49:17 +0530

    refactor: styling changes for watchlists

--- src/screens/watchlists.jsx
+++ src/screens/watchlists.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext } from 'react';
 import { UserContext } from '../contexts/userContext';
 import { useDeleteWatchlist } from '../hooks/useDeleteWatchList';
 import WatchListItem from '../components/watchListItem';
-import { Plus } from '@phosphor-icons/react';
+import { ListStar, Plus } from '@phosphor-icons/react';
 import { useCreateWatchlist } from '../hooks/useCreateWatchList';
 
 export default function Watchlists() {
@@ -14,13 +14,18 @@ export default function Watchlists() {
 	const createWatchlist = useCreateWatchlist();
 
 	return (
-		<div className='mx-auto mt-10 space-y-5'>
+		<div className='mx-auto space-y-5'>
 			<div className='flex justify-between items-center'>
-				<h1 className='font-black text-3xl'>Your Watchlists</h1>
-				<button onClick={() => createWatchlist()} className='flex items-center space-x-2 p-2 text-white bg-red-600 hover:bg-red-700 rounded'>
-					<Plus className='h-5 w-5'/>
-					<span className='hidden md:block'>New Watchlist</span>
-				</button>
+				<h1 className='font-black text-3xl text-center lg:text-left'>Your Watchlists</h1>
+				{currentUser.watchlists &&currentUser.watchlists.length > 0 && (
+					<button
+						onClick={() => createWatchlist()}
+						className='flex items-center space-x-2 p-2 text-white bg-red-600 hover:bg-red-700 rounded'
+					>
+						<Plus className='h-5 w-5' />
+						<span className='hidden md:block'>New Watchlist</span>
+					</button>
+				)}
 			</div>
 			{currentUser.watchlists && currentUser.watchlists.length > 0 ? (
 				<div className='space-y-0.5'>
@@ -34,8 +39,16 @@ export default function Watchlists() {
 					))}
 				</div>
 			) : (
-				<div className='flex flex-col items-center justify-center'>
+				<div className='w-11/12 md:w-2/3 rounded-lg flex flex-col space-y-2 items-center p-2 mx-auto'>
+					<ListStar className='w-36 h-36' />
 					<h1 className='font-black text-3xl'>No Watchlists</h1>
+					<button
+						onClick={() => createWatchlist()}
+						className='flex items-center space-x-2 p-2 text-white bg-red-600 hover:bg-red-700 rounded'
+					>
+						<Plus className='h-5 w-5' />
+						<span>New Watchlist</span>
+					</button>
 				</div>
 			)}
 		</div>

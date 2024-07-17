5fc5f6649ce71cdcabcc1f9b4d0e6cd8931928c7
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-17 15:49:56 +0530

    fix: padding on the watchlist items

--- src/components/watchListItem.jsx
+++ src/components/watchListItem.jsx
@@ -1,18 +1,19 @@
 import { Trash } from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function WatchListItem({ id, name, handleAction }) {
+	const navigate = useNavigate();
+	const handleNavigate = (id) => {
+		navigate(`/watchlist/${id}`, { state: { id } });
+	};
+
 	return (
-		<div className='w-full flex items-center justify-between p-3 hover:bg-gray-100'>
-			<Link
-				className='flex-1'
-				to={`/watchlist/${id}`}
-				state={{ id }}
-			>
+		<div className='w-full flex items-center justify-between hover:bg-gray-100'>
+			<div onClick={() => handleNavigate(id)} className='flex-1 p-3'>
 				{name}
-			</Link>
-			<button onClick={() => handleAction(id)} className='text-red-600'>
+			</div>
+			<button onClick={() => handleAction(id)} className='text-red-600 p-3'>
 				<Trash size={24} />
 			</button>
 		</div>

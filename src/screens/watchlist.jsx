459dbcefa241f8bf5c3eca0679ffd338db9ff9e7
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-17 21:54:52 +0530

    refactor: added actions in watchlist griditem

--- src/screens/watchlist.jsx
+++ src/screens/watchlist.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState, useContext } from 'react';
 import { getWatchlist } from '../contexts/db';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/userContext';
 import { useRemoveMovie } from '../hooks/useRemoveMovie';
 import GridItem from '../components/gridItem';
 import EditableInput from '../components/editableInput';
 import { toast } from 'react-toastify';
-import { FilmSlate } from '@phosphor-icons/react';
+import { Check, FilmSlate, Trash } from '@phosphor-icons/react';
+import WatchListSearch from '../components/watchListSearch';
+import { useAddMovie } from '../hooks/useAddMovie';
 
 export default function Watchlist() {
 	const location = useLocation();
+	const navigate = useNavigate();
 	const { id } = location.state;
 	const { currentUser } = useContext(UserContext);
 	const removeMovie = useRemoveMovie();
-
+	const addMovie = useAddMovie();
 	const [watchlist, setWatchlist] = useState({});
 
 	useEffect(() => {
@@ -29,7 +32,11 @@ export default function Watchlist() {
 
 	const deleteMovie = async (movie) => {
 		try {
-			setWatchlist(watchlist.movies.filter((m) => m.id !== movie.id));
+			const newMovies = watchlist.movies.filter((m) => m.Title !== movie.Title || m.imdbID !== movie.imdbID);
+			setWatchlist({
+				...watchlist,
+				movies: newMovies,
+			});
 			await removeMovie(id, movie);
 			toast.success('Movie deleted successfully');
 		} catch (error) {
@@ -57,8 +64,24 @@ export default function Watchlist() {
 						<GridItem
 							key={movie?.imdbID}
 							movie={movie}
-							currentUser={currentUser}
-							deleteMovie={deleteMovie}
+							Actions={() => (
+								<div className='flex items-center space-x-1'>
+									<button className='inline-flex rounded p-1.5 bg-red-600 hover:bg-red-600 text-white font-bold'>
+										<Check className='h-5 w-5' />{' '}
+									</button>
+									<WatchListSearch
+										currentUser={currentUser}
+										movie={movie}
+										onSelect={addMovie}
+									/>
+									<button
+										onClick={() => deleteMovie(movie)}
+										className='inline-flex rounded p-1.5 bg-red-600 hover:bg-red-600 text-white font-bold'
+									>
+										<Trash className='h-5 w-5' />
+									</button>
+								</div>
+							)}
 						/>
 					))}
 				</div>
@@ -66,7 +89,7 @@ export default function Watchlist() {
 				<div className='w-11/12 md:w-2/3 border border-gray-600 rounded-lg flex flex-col space-y-2 items-center p-2 mx-auto'>
 					<FilmSlate className='w-36 h-36' />
 					<h1 className='font-black text-3xl'>No Movies</h1>
-					<button className='p-2 bg-red-600 text-white rounded-lg hover:bg-red-700'>
+					<button onClick={() => navigate('/home')} className='p-2 bg-red-600 text-white rounded-lg hover:bg-red-700'>
 						Add Movies
 					</button>
 				</div>

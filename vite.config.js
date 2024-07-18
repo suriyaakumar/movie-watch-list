0a8d7830082687e4c11b037add53b13c54a6c8dc
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-18 12:30:45 +0530

    build: remove env prefix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		envPrefix: 'VITE_',
-	},
 	define: {
 		'import.meta.env.VITE_OMDB_KEY': JSON.stringify(
 			import.meta.env.VITE_OMDB_KEY

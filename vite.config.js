0a2a5530a706661246835d841979903bda42ddd4
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-18 12:45:12 +0530

    build: remove changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	define: {
-		'import.meta.env.VITE_OMDB_KEY': JSON.stringify(
-			import.meta.env.VITE_OMDB_KEY
-		),
-	},
 	plugins: [react()],
 });

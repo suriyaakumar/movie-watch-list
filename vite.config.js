40913fe84b136a900525d536c276ea093d64e07c
Suriyaa Kumar <believeinsurya@gmail.com>
2024-07-18 12:24:43 +0530

    build: vite config for env variables

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		envPrefix: 'VITE_',
+	},
+	define: {
+		'import.meta.env.VITE_API_KEY': JSON.stringify(
+			import.meta.env.VITE_API_KEY
+		),
+	},
 	plugins: [react()],
 });
